fa659c60c33394cf493d177fcd09fc5ac202c399
Ntsika Gajula <ntsika.gajula@gmail.com>
2026-07-06 01:55:04 +0200

    Feat: Added feature to save raw data.

--- fetch_matches.py
+++ fetch_matches.py
@@ -1,26 +1,25 @@
 import requests
+import json
+import os
+from datetime import datetime, timezone
 
 API_KEY = "9160f5c9b2a740dbac821f127b43ab60"
 BASE_URL = "https://api.football-data.org/v4"
 
-headers = {
-    "X-Auth-Token": API_KEY
-}
+
+headers = {"X-Auth-Token": API_KEY}
 
 response = requests.get(f"{BASE_URL}/competitions/WC/matches", headers=headers)
-
 data = response.json()
 
-for match in data["matches"]:
-    home = match["homeTeam"]["name"] or "TBD"
-    away = match["awayTeam"]["name"] or "TBD"
-    home_score = match["score"]["fullTime"]["home"]
-    away_score = match["score"]["fullTime"]["away"]
-    status = match["status"]
+# Create the folder to store raw files if folder doesn't exist yet
+os.makedirs("data/raw", exist_ok=True)
 
-    if home_score is None:
-        score = "vs"
-    else:
-        score = f"{home_score} - {away_score}"
+# Give the file a timestamp in its name so each run creates a new file
+timestamp = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%SZ")
+filename = f"data/raw/matches_{timestamp}.json"
 
-    print(f"{home} {score} {away} ({status})")
+with open(filename, "w") as f:
+    json.dump(data, f, indent=2)
+
+print(f"Saved {len(data['matches'])} matches to {filename}")
